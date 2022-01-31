ad3704d58da7f5092818a5ed6b163ab23620c406
fmothe <mothevv.13@gmail.com>
2022-01-31 18:42:34 -0300

    chore(Root): Se agrega sui a las dev dependencies se agrega clase 181

--- src/components/hero/HeroList.js
+++ src/components/hero/HeroList.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { getHeroesByPublisher } from '../../helpers/getHeroByPublisher';
 import { HeroCard } from './HeroCard';
+import 'animate.css'
 
 export const HeroList = ({publisher}) => {
 
 
-  const heroes = getHeroesByPublisher(publisher);
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
   return (
-    <>
+    <div className='animate__animated animate__fadeInLeft'>
       <h1 className="mt-4">{publisher} - Hero List</h1>
       <hr />
       <div className="row rows-cols-1 rows-cols-md-3 g-3">
@@ -15,6 +16,6 @@ export const HeroList = ({publisher}) => {
           <HeroCard key={hero.id} hero={hero}/>
         ))}
       </div>
-    </>
+    </div>
   );
 };
--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import { Navigate, useParams } from 'react-router-dom';
-import { getHeroById } from '../../helpers/getHeroById';
+import React, { useMemo } from "react";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { getHeroById } from "../../helpers/getHeroById";
+import 'animate.css'
 
 export const HeroScreen = () => {
+  const { heroid } = useParams();
 
-  const {heroid} = useParams();
+  const hero = useMemo(() => getHeroById(heroid), [heroid]);
+  const navigation= useNavigate()
 
+  if (!hero) return <Navigate to="/" />;
+  const imagePath = `/assets/${hero.id}.jpg`;
 
-  const hero = getHeroById(heroid);
+  const { id, superhero, characters, alter_ego, first_appearance, publisher } = hero;
 
-  if(!hero) return <Navigate to='/'/>
+  const handleBack = () => {
+    navigation(-1)
+  }
 
   return (
-    <div>
-      <h1>{hero.superhero}</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img src={imagePath} alt={superhero} className="img-thumbnail animate__animated animate__fadeInLeft" />
+      </div>
+      <div className="col-8 animate__animated animate__fadeIn">
+        <h3>{hero.superhero}</h3>
+        <ul className="list-group">
+          <li className="list-group-item">
+            {" "}
+            <b>Alter ego: </b> {alter_ego}
+          </li>
+          <li className="list-group-item"> <b> Publisher: </b>{publisher} </li>
+          <li className="list-group-item"> <b>First Appearence: </b>{first_appearance}</li>
+        </ul>
+        <h5 className="mt-3">Characters</h5>
+        <p>{characters}</p>
+        <button className="btn btn-primary justify-content-end" onClick={handleBack}>Back</button>
+      </div>
     </div>
   );
 };
